c3f6e75bfdbda1ef61e68c4a229b26d7e2229158
mitsuru793 <mitsuru793@gmail.com>
2018-04-29 22:49:17 +0900

    extract load config with method

--- lib/pin_note/cli.rb
+++ lib/pin_note/cli.rb
@@ -6,18 +6,15 @@ module PinNote
     desc "save Here is note... #tag", "Save a note."
     option :category, aliases: :c, type: :string, desc: 'Save note on CATEGORY'
 
-    def save(*word)
-      if File.exist?(config_path)
-        saved = YAML.load_file(config_path)
-      else
-        saved = []
-      end
+    def save(word, *words)
+      words.unshift(word)
 
       note = Note.new(
-          note: word.join(' '),
+          note: words.join(' '),
           category: options[:category] || ENV['PIN_NOTE_CATEGORY'] || nil,
           created_at: Time.now,
           )
+      saved = load_saved
       saved.push(note.to_h)
 
       File.open(config_path, 'w') do |f|
@@ -28,6 +25,16 @@ module PinNote
     private
     def config_path
       File.expand_path('~/.pin-note.yml')
+    end
+
+    def load_saved
+      if File.exist?(config_path)
+        saved = YAML.load_file(config_path)
+      else
+        saved = []
+      end
+
+      saved
     end
   end
 end
