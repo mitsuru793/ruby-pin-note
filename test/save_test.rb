9306f5b2463b60e74d3c715b059d12ca3fb8660a
mitsuru793 <mitsuru793@gmail.com>
2018-04-29 19:34:49 +0900

    extract test code with run_command

--- test/save_test.rb
+++ test/save_test.rb
@@ -2,53 +2,49 @@ require "test_helper"
 
 class SaveTest < Minitest::Test
   def setup
-    FakeFS.activate!
-    FileUtils.mkdir_p(File.expand_path('~'))
-  end
-
-  def teardown
-    FakeFS.deactivate!
+    @now = Time.new
   end
 
   def test_save_word_without_category
-    home = File.expand_path('~')
-
-    config_path = File.join(home, '.pin-note.yml')
-    now = Time.now
-
-    Time.stub :now, now do
-      PinNote::Cli.start(%w[save Hello world!])
-    end
+    saved = run_command(%w[save Hello world!])
 
     expected = [
         {
             id: 1,
             note: 'Hello world!',
             category: nil,
-            created_at: now.to_s,
+            created_at: @now.to_s,
         }
     ]
-    assert_equal(expected, YAML.load_file(config_path))
+    assert_equal(expected, saved)
   end
 
   def test_save_word_with_category
-    home = File.expand_path('~')
-
-    config_path = File.join(home, '.pin-note.yml')
-    now = Time.now
-
-    Time.stub :now, now do
-      PinNote::Cli.start(%w[save --category greet Hello world!])
-    end
+    saved = run_command(%w[save --category greet Hello world!])
 
     expected = [
         {
             id: 1,
             note: 'Hello world!',
             category: 'greet',
-            created_at: now.to_s,
+            created_at: @now.to_s,
         }
     ]
-    assert_equal(expected, YAML.load_file(config_path))
+    assert_equal(expected, saved)
+  end
+
+  private
+  def run_command(args, config_path = '~/.pin-note.yml')
+    saved = nil
+    FakeFS do
+      FileUtils.mkdir_p(File.expand_path('~'))
+
+      Time.stub :now, @now do
+        PinNote::Cli.start(args)
+      end
+
+      saved = YAML.load_file(File.expand_path(config_path))
+    end
+    saved
   end
 end
